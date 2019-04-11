a84052df0305d1046631ac5a6845271b4727fc03
Brian <brian.gyzels10@gmail.com>
2019-04-11 18:13:13 +0200

    GetGazetteerPolygons added

--- species_Clustering.R
+++ species_Clustering.R
@@ -1,12 +1,12 @@
 install.packages("proxy")
 install.packages("jsonlite")
 install.packages("stringr")
-
+install.packages("raster")
 library('httr')
 library('data.table')
 
 library("robis")
-
+library("maptools")
 library(rgeos)
 library('leaflet')
 library("httr")
@@ -38,32 +38,32 @@ ShapeLME <- mr_shp(
 distribution <- wm_distribution(wm_name2id("Abra alba"))
 distinct_distribution <-sqldf("select distinct(locationID),locality from distribution")
 wm_record(id = 105706)
+polygonDF <- SpatialPolygonsDataFrame(SpatialPolygons(list()))
 gazetteerPolygonDF<- data.frame( MGRID=character(),
                   GazetteerName=character(),
                  OverlappingMEOW=numeric(),
                  OverlappingLME=numeric(),
                  OverlappingEEZ = numeric(),
                  NumberOfObs= numeric(),
-                 Size = numeric()
+                 Size = numeric(),stringsAsFactors=FALSE
                  ) 
   
-for (i in 3:3){
+for (i in 1:1){
 MGRID <- str_split(distinct_distribution[i,]$locationID,"/",simplify = TRUE)
 response <- GET(url = paste("http://www.marineregions.org/rest/getGazetteerWMSes.json/",MGRID[5],"/",sep="") )
 record <- content(response)
-
+i
 shapeprobeer <- mr_shp(
   key = paste(record[[1]]$namespace,record[[1]]$featureType, sep = ":"),  maxFeatures = 500, read = TRUE
 )
+filterd <-shapeprobeer[toupper(shapeprobeer@data[,record[[1]]$featureName]) == toupper(record[[1]]$value),]
  
-filterd <-shapeprobeer[toupper(shapeprobeer@data$polygon_nm) == toupper(distinct_distribution[i,]$locality),]
-
-
+sizeGazetteerRegion <- gArea(filterd)
 #gazetteerPolygonDF.MGRID
-gazetteerPolygonDF[i,]$MGRID = MGRID[5]
+gazetteerPolygonDF[i,]$MGRID <- MGRID[5]
 
 #gazetteerPolygonDF.
-gazetteerPolygonDF[i,]$GazetteerName = distinct_distribution[i,]$locality
+gazetteerPolygonDF[i,]$GazetteerName <- distinct_distribution[i,]$locality
 
 #gazetteerPolygonDF.OverlappingMEOW
 intersectMEOW <- gIntersection(filterd,shapeEcoregions)
@@ -82,7 +82,7 @@ listObsInPolygon <- sp::over(coords,as.SpatialPolygons.PolygonsList(filterd@poly
 gazetteerPolygonDF[i,]$NumberOfObs <- sum(lapply(listObsInPolygon,length)>0)
 
 #gazetteerPolygonDF.Size
-gazetteerPolygonDF[i,]$Size <- gArea(filterd)
+gazetteerPolygonDF[i,]$Size <- sizeGazetteerRegion
 
  sum <- sum + 1
 }
@@ -90,7 +90,7 @@ gazetteerPolygonDF[i,]$Size <- gArea(filterd)
 #   key = "MarineRegions:eez" ,maxFeatures = 500 
 # ) 
  
-
+ 
 # filterd <- shapeprobeer3[shapeprobeer3$polygon_nm == "OOSTENDE", ]
 # names(shapeprobeer3@data)
           
@@ -104,3 +104,30 @@ leaflet() %>%
 dist_d <- dist(d[1:30000,],method='euclidean')
 sapply(d, function(x) sum(is.na(x)))
 
+#GetShapeFilesOfGazetteer
+ 
+
+for (i in 1:2){
+  MGRID <- str_split(distinct_distribution[i,]$locationID,"/",simplify = TRUE)
+  response <- GET(url = paste("http://www.marineregions.org/rest/getGazetteerWMSes.json/",MGRID[5],"/",sep="") )
+  record <- content(response)
+  i
+  shapeprobeer <- mr_shp(
+    key = paste(record[[1]]$namespace,record[[1]]$featureType, sep = ":"),  maxFeatures = 500, read = TRUE
+  )
+  if (i>1){
+    newGazetteerPolygon  <-shapeprobeer[toupper(shapeprobeer@data[,record[[1]]$featureName]) == toupper(record[[1]]$value),]
+    names(gazetteerPolygons) <- names(newGazetteerPolygon) 
+    
+     gazetteerPolygons <- rbind(gazetteerPolygons, newGazetteerPolygon, makeUniqueIDs = TRUE) 
+  }else
+  {
+       gazetteerPolygons <-shapeprobeer[toupper(shapeprobeer@data[,record[[1]]$featureName]) == toupper(record[[1]]$value),]
+ 
+  }
+  
+  }
+#plotting gazetteer
+leaflet() %>%
+  addTiles()%>%
+  addPolygons(data = gazetteerPolygons)   
