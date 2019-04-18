5a8848a799f35829d7201335462fb55b8bdc1d25
Bruno <bruno.kanage@hotmail.com>
2019-04-18 16:14:50 +0200

    nieuw

--- species_density_distributions.R
+++ species_density_distributions.R
@@ -19,6 +19,26 @@ library(worrms)
 library(stringr)
 library(sqldf)
 
+install.packages("mvShapiroTest")
+library(mvShapiroTest)
+z <- data.frame(x, y)
+z <- data.matrix(z)
+p <- kde(z)
+plot(p, display="filled.contour2", cont=c(25,50,75), xlim=c(-20,20), ylim=c(45,65))
+
+plot(p)
+mvShapiro.Test(head(z,5000))
+mvShapiro.Test(z[5000:9999])
+mvShapiro.Test(z[9999:14998])
+mvShapiro.Test(z[14998:19997])
+
+qqnorm(data$decimalLatitude)
+qqline(data$decimalLatitude)
+log.dla <-log(data$decimalLatitude)
+qqnorm(log.dla)
+qqline(log.dla)
+qqnorm(data$decimalLongitude)
+qqline(data$decimalLongitude)
 
 #Witte dunschaal
 data <- occurrence("Abra alba") 
@@ -53,11 +73,24 @@ NSM <- ggmap(northseamap, extent = "normal", maprange = FALSE) +
                fill = "orange", colour = "red", alpha = 0.5)
 plot1 <- NSM + stat_density2d(
   aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.3),
-  size = 0.5, bins = 800, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+  size = 0.5, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
 plot2<- NSM + stat_density2d(
   aes(x = ABAL_aqm$Long, y = ABAL_aqm$Lat, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 8, data = ABAL_aqm, geom = "polygon") + geom_density2d(data = ABAL_aqm, aes(x=ABAL_aqm$Long, y = ABAL_aqm$Lat), size = 0.1) 
+  size = 0.2, h=1, data = ABAL_aqm, geom = "polygon") + geom_density2d(data = ABAL_aqm, aes(x=ABAL_aqm$Long, y = ABAL_aqm$Lat), size = 0.1) 
 grid.arrange(plot1, plot2, ncol=2)
+
+NSM <- ggmap(northseamap, extent = "normal", maprange = FALSE) +
+  geom_polygon(data = shape,
+               aes(long, lat, group = group), 
+               fill = "orange", colour = "red", alpha = 0.5)
+plot1 <- NSM + stat_density2d(
+  aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.3),
+  size = 0.5, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+plot2<- NSM + stat_density2d(
+  aes(x = ABAL_aqm$Long, y = ABAL_aqm$Lat, fill = ..level.., alpha = 0.1),
+  size = 0.2, h=1, data = ABAL_aqm, geom = "polygon") + geom_density2d(data = ABAL_aqm, aes(x=ABAL_aqm$Long, y = ABAL_aqm$Lat), size = 0.1) 
+grid.arrange(plot1, plot2, ncol=2)
+
 
 
 
@@ -80,10 +113,10 @@ northseamap2 <- get_map(location = "north sea",  maptype = "satellite", source =
 NSM2 <- ggmap(northseamap2)
 plot1 <- NSM2 + stat_density2d(
   aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.1),
-  size = 0.5, bins = 80, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+  size = 0.5, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
 plot2<- NSM2 + stat_density2d(
   aes(x = SCCA$Long, y = SCCA$Lat, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 80, data = SCCA, geom = "polygon") + geom_density2d(data = SCCA, aes(x=SCCA$Long, y = SCCA$Lat), size = 0.1) 
+  size = 0.2, h=1, data = SCCA, geom = "polygon") + geom_density2d(data = SCCA, aes(x=SCCA$Long, y = SCCA$Lat), size = 0.1) 
 grid.arrange(plot1, plot2, ncol=2)  
 
 #Gewone zeehond
@@ -92,10 +125,10 @@ northseamap3 <- get_map(location = "north atlantic ocean",  maptype = "satellite
 NSM2 <- ggmap(northseamap3)
 plot1 <- NSM2 + stat_density2d(
   aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+  size = 0.2, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
 plot2<- NSM2 + stat_density2d(
   aes(x = PHVI$Long, y = PHVI$Lat, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = PHVI, geom = "polygon") + geom_density2d(data = PHVI, aes(x=PHVI$Long, y = PHVI$Lat), size = 0.1) 
+  size = 0.2, h=1, data = PHVI, geom = "polygon") + geom_density2d(data = PHVI, aes(x=PHVI$Long, y = PHVI$Lat), size = 0.1) 
 grid.arrange(plot1, plot2, ncol=2)  
 
 #Jan-van-gent
@@ -104,10 +137,10 @@ northseamap3 <- get_map(location = "north atlantic ocean",  maptype = "satellite
 NSM2 <- ggmap(northseamap3)
 plot1 <- NSM2 + stat_density2d(
   aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+  size = 0.2, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
 plot2<- NSM2 + stat_density2d(
   aes(x = MOBA$Long, y = MOBA$Lat, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = MOBA, geom = "polygon") + geom_density2d(data = MOBA, aes(x=MOBA$Long, y = MOBA$Lat), size = 0.1) 
+  size = 0.2, h=1, data = MOBA, geom = "polygon") + geom_density2d(data = MOBA, aes(x=MOBA$Long, y = MOBA$Lat), size = 0.1) 
 grid.arrange(plot1, plot2, ncol=2)  
 
 #Oorkwal
@@ -116,10 +149,10 @@ northseamap4 <- get_map(location = "world",  maptype = "satellite", source = "go
 NSM2 <- ggmap(northseamap4)
 plot1 <- NSM2 + stat_density2d(
   aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+  size = 0.2, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
 plot2<- NSM2 + stat_density2d(
   aes(x = AUAU$Long, y = AUAU$Lat, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = AUAU, geom = "polygon") + geom_density2d(data = AUAU, aes(x=AUAU$Long, y = AUAU$Lat), size = 0.1) 
+  size = 0.2, h=1, data = AUAU, geom = "polygon") + geom_density2d(data = AUAU, aes(x=AUAU$Long, y = AUAU$Lat), size = 0.1) 
 grid.arrange(plot1, plot2, ncol=2)  
 
 #Lederschildpad
@@ -128,10 +161,10 @@ northseamap4 <- get_map(location = "world",  maptype = "satellite", source = "go
 NSM2 <- ggmap(northseamap4)
 plot1 <- NSM2 + stat_density2d(
   aes(x = data$decimalLongitude, y = data$decimalLatitude, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
+  size = 0.2, h=5, data = data, geom = "polygon") + geom_density2d(data = data, aes(x=data$decimalLongitude, y = data$decimalLatitude), size = 0.1)   
 plot2<- NSM2 + stat_density2d(
   aes(x = DECO$Long, y = DECO$Lat, fill = ..level.., alpha = 0.1),
-  size = 0.2, bins = 100, data = DECO, geom = "polygon") + geom_density2d(data = DECO, aes(x=DECO$Long, y = DECO$Lat), size = 0.1) 
+  size = 0.2, h=1, data = DECO, geom = "polygon") + geom_density2d(data = DECO, aes(x=DECO$Long, y = DECO$Lat), size = 0.1) 
 grid.arrange(plot1, plot2, ncol=2)  
 
 
