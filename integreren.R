fb84f0141b4de8584eaae61405e221986b1352d1
Brian <brian.gyzels10@gmail.com>
2019-04-19 14:43:19 +0200

    integreren data frame added with (obsID,LMEid,x,y)

--- integreren.R
+++ integreren.R
@@ -1,38 +1,65 @@
+library("aquamapsdata")
+library("purrr")
+
+library("igraph")
+library("devtools")
+library(rgdal)
+library(maptools)
+library(sp)
+
+library('httr')
+library('data.table')
+
+library("worrms")
+library(rgeos)
+library("mregions")
+library("robis")
+library('leaflet')
+library("sqldf")
+library(dplyr)
+
+#waarneming ophalen en omzetten naar spatialpoints
+
+data <- occurrence("Abra alba") 
+d <- data.frame(data$decimalLongitude,data$decimalLatitude)
+coords <-SpatialPoints(d, proj4string=CRS(as.character(NA)), bbox = NULL)
+
+#Waarnemingen dat binnen LME vallen + dataset connectie LME polygoon ID en waarneming ID
+
+spatialpolygonsListLME<- as.SpatialPolygons.PolygonsList(shapeLME@polygons, proj4string=CRS(as.character(NA)))  
+obsJoinLME<- sp::over(coords,spatialpolygonsListLME,returnList = TRUE)
+obsInLME <-  obsJoinLME[lapply(obsJoinLME,length)>0]
+ObsDataInLME <-data[names(obsInLME),]
+
+
+
+O2 <- data.frame( ID = names(obsInLME),
+                  LMEid = unlist(obsInLME ),
+                    x= ObsDataInLME[,"decimalLongitude" ],
+                  y= ObsDataInLME[,"decimalLatitude" ],
+                  stringsAsFactors=FALSE
+) 
+
 
 
 #ID van observaties die overlappen met LME polygonen
 
 
-d <- data.frame(ObsDataInLME$decimalLongitude,ObsDataInLME$decimalLatitude)
-coords <-SpatialPoints(d, proj4string=CRS(as.character(NA)), bbox = NULL)
-
-
-#Waarnemingen dat binnen LME vallen + dataset connectie LME polygoon ID en waarneming ID
-
-spatialpolygonsListLME<- as.SpatialPolygons.PolygonsList(shp@polygons, proj4string=CRS(as.character(NA)))  
-obsJoinLME<- sp::over(coords,spatialpolygonsListLME,returnList = TRUE)
-obsInLME <-  obsJoinLME[lapply(obsJoinLME,length)>0]
-ObsDataInLME <-data[names(obsInLME),]
-
-coordinates(d) = ~ObsdecimalLongitude+decimalLatitude
-
-
 # return the number of points in each polygon:
-sapply(over(spatialpolygonsListLME, geometry(data), returnList = TRUE), length)
+ 
+# O2 <- over(coords,spatialpolygonsListLME, returnList = TRUE) #ID => z-waarden van opzoeken
+ 
+
+ 
+
+coordinates(d) = ~ x + y
 
 
-O1<- over(spatialpolygonsListLME, data, returnList = TRUE)
-O2 <- over(spatialpolygonsListLME, geometry(data), returnList = TRUE) #ID => z-waarden van opzoeken
-O3 <- over(data, spatialpolygonsListLME)
-O4<- over(data, shp)
 
-w<- O2[["0"]]
-colnames(d)[2] <- "x"
-colnames(d)[3] <- "y"
 
 ## eerst een join met de ids van de observaties die vallen in de polygonen en de ids van de dataset d waar de x en y coordinaten staan
-library(dplyr)
-res <- d %>% left_join(w, by = c("ID", )) %>%
+
+res <- d %>% left_join(w, by = c("ID" )) %>%
   mutate(Values=coalesce(Values.x,Values.y)) %>%
   select(-Values.x,-Values.y)
 
