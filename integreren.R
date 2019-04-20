bd6fd3126ac201e3c7873a3de6f49101e2e7ce9d
Bruno <bruno.kanage@hotmail.com>
2019-04-20 20:27:37 +0200

    update

--- integreren.R
+++ integreren.R
@@ -1,52 +1,51 @@
+## sample data: bivariate normal, with covariance/correlation 0
+x <- data$decimalLongitude
+x<- na.omit(x)
+y<- data$decimalLatitude
+y <- na.omit(y)
+## load MASS
+library(MASS)
+
+## domain:
+xlim <- range(x)
+ylim <- range(y)
+## 2D Kernel Density Estimation
+den <- kde2d(x, y, n = 116, lims = c(xlim, ylim)) #gridpoints=> zoals xlim, mooie range over x
+##persp(den$x,den$y,den$z)
+z <- den$z  ## extract density
+#nieuwe tabel maken met alle mogelijke x,y combinaties en z-waarden => zdata
+zdata <- read_excel("C:/Users/bruno/OneDrive/Bureaublad/zdata.xlsx")
+test <- na.omit(zdata$z)
+sum(test) #2.64 ipv 1 => normaliseren?
 
 
-#ID van observaties die overlappen met LME polygonen
-
-
-d <- data.frame(ObsDataInLME$decimalLongitude,ObsDataInLME$decimalLatitude)
-coords <-SpatialPoints(d, proj4string=CRS(as.character(NA)), bbox = NULL)
-
-
-#Waarnemingen dat binnen LME vallen + dataset connectie LME polygoon ID en waarneming ID
-
-spatialpolygonsListLME<- as.SpatialPolygons.PolygonsList(shp@polygons, proj4string=CRS(as.character(NA)))  
+##select IDS of grid points that are in LMEs
+d <- data.frame(den$x,den$y)
+zdata2 <-cbind(zdata$x, zdata$y)
+zdata2 <- data.frame(zdata2)
+coords <-SpatialPoints(zdata2, proj4string=CRS(as.character(NA)), bbox = NULL)
+shapeLME <- mr_shp(
+  key = "MarineRegions:lme" ,maxFeatures = 500 
+)
+spatialpolygonsListLME<- as.SpatialPolygons.PolygonsList(shapeLME@polygons, proj4string=CRS(as.character(NA)))  
 obsJoinLME<- sp::over(coords,spatialpolygonsListLME,returnList = TRUE)
 obsInLME <-  obsJoinLME[lapply(obsJoinLME,length)>0]
-ObsDataInLME <-data[names(obsInLME),]
+ObsDataInLME <-zdata[names(obsInLME),]
+O2 <- data.frame( ID = names(obsInLME), LMEid= unlist(obsInLME), x= ObsDataInLME[, "x" ], y=ObsDataInLME[, "y" ], stringsAsFactors = FALSE)
+View(O2)
 
-coordinates(d) = ~ObsdecimalLongitude+decimalLatitude
+## get z-values of these ID's
 
-
-# return the number of points in each polygon:
-sapply(over(spatialpolygonsListLME, geometry(data), returnList = TRUE), length)
-
-
-O1<- over(spatialpolygonsListLME, data, returnList = TRUE)
-O2 <- over(spatialpolygonsListLME, geometry(data), returnList = TRUE) #ID => z-waarden van opzoeken
-O3 <- over(data, spatialpolygonsListLME)
-O4<- over(data, shp)
-
-w<- O2[["0"]]
-colnames(d)[2] <- "x"
-colnames(d)[3] <- "y"
-
-## eerst een join met de ids van de observaties die vallen in de polygonen en de ids van de dataset d waar de x en y coordinaten staan
+ID <- 1:13456
+zdata <- cbind(ID, zdata)
+View(zdata)
 library(dplyr)
-res <- d %>% left_join(w, by = c("ID", )) %>%
-  mutate(Values=coalesce(Values.x,Values.y)) %>%
-  select(-Values.x,-Values.y)
+m<- merge(zdata, O2, by="ID", all.y=TRUE)
+View(m)
+m <- data.frame(m)
 
-
-## z-value ophalen van coordinaten die opverlappen met polygoon
-
-library(MASS)
-f <- kde2d(x,y, h=5, n=20) # met n= grid points
-df2 <- do.call(rbind.data.frame, f) # check the z-values of the kde2d function
-
-## daarna nog eens een join met df2 (alle x en y van res moeten gelijk zijn met x en y van df2)
-res2 <- res %>% left_join(df2, by = c("ID", )) %>%
-  mutate(Values=coalesce(Values.x,Values.y)) %>%
-  select(-Values.x,-Values.y)
-
-## tot slot nemen we de som van alle z-waarden van res2
-
+##summarize per LME
+x<- xtabs(m$z~m$LMEid)
+x
+plot(x)
+categories <- unique(m$LMEid)
